461d1e260615b0dd5327221faf7179604065bf2d
Arul Jayaraj <aruljayarajj826@gmail.com>
2025-08-21 15:52:41 +0530

    Your commit message

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
 from fastapi import FastAPI
 import uvicorn
+from fastapi.middleware.cors import CORSMiddleware
 from routers.user_info import router
 
 app = FastAPI()
 
 app.include_router(router,prefix="/skillwell/v1/logapi", tags=["skillwell"])
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 
 if __name__ == "__main__":
