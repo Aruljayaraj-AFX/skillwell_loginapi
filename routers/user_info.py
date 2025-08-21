ae1b4e656fbdf7333e0b073acfdf0e14857b6429
Arul Jayaraj <aruljayarajj826@gmail.com>
2025-08-21 21:25:34 +0530

    Your commit message

--- routers/user_info.py
+++ routers/user_info.py
@@ -7,10 +7,10 @@ router= APIRouter()
 async def login(res:str=Depends(user_login())):
     return res
 
-@router.get("/add_user")
+@router.post("/add_user")
 async def add_user(adduser:str=Depends(user_add())):
     return adduser
 
-@router.get("/password_change")
+@router.put("/password_change")
 async def pass_ch(newpass:str=Depends(pass_change())):
     return newpass
